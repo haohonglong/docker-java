fa1f18bfdaba9b0874a774948e21f030f918f8a2
haohonglong <947923503@qq.com>
2025-09-17 11:14:03 +0800

    modified

--- debugger.sh
+++ debugger.sh
@@ -1,4 +1,6 @@
 
 #!/bin/bash
 
+# mvn clean package && java -Dfile.encoding=UTF-8 -jar ./target/snapshot.jar
+
 mvn clean package && java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005 -jar ./target/debugger.jar
